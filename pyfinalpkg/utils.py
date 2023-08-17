3e27ebcaaadc31a2c47323b5ed9b425caeac112a
Nisha Ranga <98541911+nranga-ko@users.noreply.github.com>
2023-08-17 11:01:28 -0400

    Update utils.py

--- pyfinalpkg/utils.py
+++ pyfinalpkg/utils.py
@@ -2,16 +2,14 @@
 # UTILS
 #----------------------------
 
-#import pandas as pd
+import pandas as pd
 from datetime import date
 
     
 def read_csv_file(file_path: str):
     print('Reading CSV file')
-    
-    #df = pd.read_csv(file_path)
-
-    #return df
+    df = pd.read_csv(file_path)
+    return df
 
 def get_file_name():
     print("Inside 'UTILS' FILE..!!")
